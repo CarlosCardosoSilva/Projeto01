056c04a687bf7d57f973bb1af8781df51e21f518
CarlosCardosoSilva <juvenalcardoso961@gmail.com>
2026-03-30 18:46:28 -0300

    criação da classe entity

---
+++ desafio-todo-list/src/main/java/com/example/desafio_todo_list/entity/Todo.java
@@ -0,0 +1,54 @@
+package com.example.desafio_todo_list.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "todos")
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String dscricao;
+    private boolean realizado;
+    private int prioridade;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getDscricao() {
+        return dscricao;
+    }
+    public void setDscricao(String dscricao) {
+        this.dscricao = dscricao;
+    }
+    public boolean isRealizado() {
+        return realizado;
+    }
+    public void setRealizado(boolean realizado) {
+        this.realizado = realizado;
+    }
+    public int getPrioridade() {
+        return prioridade;
+    }
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+
+
+        
+}
